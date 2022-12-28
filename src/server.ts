2a9ec8241832552c6894d1cfc99396d00d8433da
fps <anthony.nnaemeka.umeh@gmail.com>
2022-12-28 14:33:17 +0100

    This was hot.

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,34 @@
 // E X P R E S S   S E R V E R //
 
 import express from "express"
+import { Db, MongoClient } from "mongodb"
+
 const app = express()
 app.use(express.json())
 
 const PORT: number = 8080
+const URL: string = "mongodb://127.0.0.1:27017/"
 
-app.listen(PORT, () => {
+const client: MongoClient = new MongoClient(URL)
+
+app.listen(PORT, "", 10, () => {
     console.log(`Server running at 127.0.0.1:${PORT}!`)
+
+    client.connect((err, db) => {
+        if (err) throw err
+        console.log(`Connected @ ${URL}!`)
+
+        const database = db?.db("ourregister")
+        database?.createCollection("users", (err, db) => {
+            if (err) {
+                console.log("Collection users, exists!")
+            } else {
+                console.log("Collection users, created")
+            }
+        })
+        
+        client.close()
+    })
 })
 
 app.get("/", (req, res) => {
